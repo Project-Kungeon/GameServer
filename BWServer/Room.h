751eb1e5f2f083cd94d31875fa6a1ca5c7f31662
peace0096 <peace0096@naver.com>
2024-01-14 03:24:12 +0900

    [추가] 세션 컨텐츠 코드, Message Protobuf 클래스, GameServer 클래스 추가

--- BWServer/Room.h
+++ BWServer/Room.h
@@ -5,9 +5,19 @@ using namespace std;
 
 class Room
 {
-	void Join(MemberPtr member);
-	void Leave(MemberPtr member);
-	void Broadcast(asio::mutable_buffer& buffer);
+public:
+	void Join(MemberPtr member)
+	{
+
+	}
+	void Leave(MemberPtr member)
+	{
+
+	}
+	void Broadcast(asio::mutable_buffer& buffer)
+	{
+
+	}
 
 private:
 	set<MemberPtr> _members;
