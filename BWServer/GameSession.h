c4a9cc5ce76e755ebc3c76413aa75cb834cedc32
peace0096 <peace0096@naver.com>
2024-02-13 00:29:19 +0900

    [버그 수정] static_assert failed: 'atomic<T> requires T to be trivially copyable, copy constructible, move constructible, copy assignable, and move assignable. 해결 완료

--- BWServer/GameSession.h
+++ BWServer/GameSession.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "pch.h"
+#include "Player.h"
+#include <memory>
 
 class GameSession : public Session
 {
@@ -8,7 +10,7 @@ public:
 	virtual void OnDisconnected() override;
 
 private:
-	atomic<boost::shared_ptr<Player>> player;
+	atomic<std::shared_ptr<Player>> player;
 	
 };
 
