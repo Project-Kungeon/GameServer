763060c66a44d631b34b6ab75466bd58838e2c2d
peace0096 <peace0096@naver.com>
2024-02-18 01:30:06 +0900

    [추가] 플레이어가 방 접속 시, 모든 클라이언트에게 Spawn 패킷을 브로드캐스트하는 기능 추가

--- BWServer/Room.h
+++ BWServer/Room.h
@@ -2,14 +2,25 @@
 #include "Member.h"
 using namespace std;
 
+extern RoomPtr GRoom[UINT16_MAX];
+
 class Room
 {
 public:
-	void Join(MemberPtr member);
-	void Leave(MemberPtr member);
-	void Broadcast(asio::mutable_buffer& buffer);
+	void init();
+	bool Join(ObjectPtr object);
+	bool Leave(ObjectPtr object);
+	void Broadcast(asio::mutable_buffer& buffer, unsigned int exceptId);
+
+	bool HandleEnterPlayer(PlayerPtr player);
+	bool HandleLeavePlayer(PlayerPtr player);
+	void HandleMovePlayer(PlayerPtr player);
+
+private:
+	bool AddObject(ObjectPtr object);
+	bool RemoveObject(unsigned int objectId);
 
 private:
 	set<MemberPtr> _members;
+	unordered_map<unsigned int, ObjectPtr> _objects;
 };
-typedef std::shared_ptr<Room> RoomRef;
--- BWServer/RoomPacketHandler.h
+++ BWServer/RoomPacketHandler.h
@@ -1,7 +1,7 @@
 #pragma once
 class RoomPacketHandler
 {
-
+public:
 	static bool Handle_C_EnterRoom(SessionPtr& session, message::C_EnterRoom& pkt);
 };
 
