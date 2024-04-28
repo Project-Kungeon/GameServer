08f547a225731d93ca9d6679fc1c7e775b374fd8
peace0096 <peace0096@naver.com>
2024-04-28 23:31:31 +0900

    [수정] 큰 용량들 수정

--- BWServer/RoomPacketHandler.cpp
+++ BWServer/RoomPacketHandler.cpp
@@ -21,7 +21,7 @@ bool RoomPacketHandler::Handle_C_EnterRoom(SessionPtr& session, message::C_Enter
 bool RoomPacketHandler::Handle_C_Move(SessionPtr& session, message::C_Move& pkt)
 {
 	// cast into GameSession ( for getting object info)
- 	GameSessionPtr gameSession = static_pointer_cast<GameSession>(session);
+	GameSessionPtr gameSession = static_pointer_cast<GameSession>(session);
 
 	// 플레이어 정보가 들어오지 않았다면 일단 패스
 	if (!gameSession->isEnterGame) return false;
