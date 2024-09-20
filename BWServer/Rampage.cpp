aa95f45851261a29a253bd79ef8120ddd481034a
peace0096 <peace0096@naver.com>
2024-09-21 01:43:50 +0900

    [수정] 램페이지 패트롤 Tree 구성 중

--- BWServer/Rampage.cpp
+++ BWServer/Rampage.cpp
@@ -2,13 +2,15 @@
 #include "Rampage.h"
 
 Rampage::Rampage()
-	: Monster(message::MONSTER_TYPE_RAMPAGE, 1000, 1000, 100)
+	: Monster(message::MONSTER_TYPE_RAMPAGE, 1000, 1000, 100), Tree(new RampageTree(static_pointer_cast<Rampage>(shared_from_this())))
 {
+	Tree->Init();
 }
 
 Rampage::Rampage(float maxHp, float hp, float dropExp)
-	: Monster(message::MONSTER_TYPE_RAMPAGE, maxHp, hp, dropExp)
+	: Monster(message::MONSTER_TYPE_RAMPAGE, maxHp, hp, dropExp), Tree(new RampageTree(static_pointer_cast<Rampage>(shared_from_this())))
 {
+	Tree->Init();
 }
 
 Rampage::~Rampage()
@@ -17,18 +19,7 @@ Rampage::~Rampage()
 
 void Rampage::Tick(uint32 DeltaTime)
 {
-	DetectCooldown += DeltaTime;
-
-	// Detect Player..
-	if (DetectCooldown >= DETECT_COOLTIME)
-	{
-		DetectCooldown = 0;
-		if (FindClosePlayer())
-		{
-			spdlog::debug("Rampage found Player {}", CloseTarget.lock()->GetObjectId());
-		}
-	}
-
+	Tree->Tick(DeltaTime);
 	
 }
 
@@ -41,4 +32,93 @@ bool Rampage::FindClosePlayer()
 	*/
 
 	return CloseTarget.lock() != nullptr;
+}
+
+double Rampage::GetDistanceToTarget(std::weak_ptr<Creature> Target)
+{
+	if (auto creature = Target.lock())
+	{
+		Location SelfLoc = GetLocation();
+		Location TargetLoc = creature->GetLocation();
+
+		return sqrt(pow(abs((int)(SelfLoc.x - TargetLoc.x)), 2) +
+			pow(abs((int)(SelfLoc.y - TargetLoc.y)), 2) +
+			pow(abs((int)(SelfLoc.z - TargetLoc.z)), 2));
+	}
+
+	return -1;
+}
+
+bool Rampage::RegularPattern()
+{
+	if (phase == 1)
+	{
+		Roar();
+		return true;
+	}
+	else if (phase == 2)
+	{
+		EarthQuake();
+		return true;
+	}
+	return false;
+}
+
+bool Rampage::UseSkillToAggro()
+{
+	// AggroTarget이 존재한다면 스킬을 사용합니다. 쿨타임을 계산합니다.
+	double distance = GetDistanceToTarget(AggroTarget);
+
+	if (distance > 500)
+	{
+		ThrowAway();
+		return true;
+	}
+	else if (distance >= 0)
+	{
+		EnhancedAttack();
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+
+	return false;
+
+}
+
+bool Rampage::BasicAttack()
+{
+	if (auto Target = CloseTarget.lock())
+	{
+		// TODO : BA Packet
+	}
+
+	return false;
+}
+
+bool Rampage::CanNotAttack()
+{
+	return false;
+}
+
+void Rampage::Roar()
+{
+}
+
+void Rampage::EarthQuake()
+{
+}
+
+void Rampage::TurnToTarget(std::weak_ptr<Creature> Target)
+{
+}
+
+void Rampage::ThrowAway()
+{
+}
+
+void Rampage::EnhancedAttack()
+{
 }
---
+++ BWServer/RampageTree.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "pch.h"
+#include "Selector.h"
+
+class RampageTree
+{
+public:
+    RampageTree(std::weak_ptr<Rampage> rampage);
+    virtual ~RampageTree();
+
+public:
+    void Init();
+    void MakeRootSelector();
+    void MakeFindPlayerSelector();
+    void MakeCanAttackSelector();
+    void Tick(uint32 DeltaTime);
+
+private:
+    Selector* RootSelector;
+    Selector* FindPlayerSelector;
+    Selector* CanAttackSelector;
+
+    std::weak_ptr<Rampage> rampage;
+
+    // Timer For Behavior
+    uint32 DetectCooldown = 0;
+    uint32 RegularPatternCooldown = 0;
+    uint32 SkillAttackCooldown = 0;
+    uint32 BasicAttackCooldown = 0;
+
+    // const
+private:
+    const uint32 DETECT_COOLTIME = 990;
+    const uint32 RP_COOLTIME = 3970;
+    const uint32 SKILL_COOLTIME = 4960;
+    const uint32 BA_COOLTIME = 1450;
+};
