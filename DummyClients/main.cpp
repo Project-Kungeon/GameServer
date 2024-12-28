a8d3dd92923bac95a3ede4b0b3bb13fb85224c05
peace0096 <peace0096@naver.com>
2024-12-29 01:00:40 +0900

    [수정] 스트레스 테스트 접속되는 것 확인

--- DummyClients/main.cpp
+++ DummyClients/main.cpp
@@ -1,6 +1,14 @@
 ﻿#include "pch.h"
+#include "Test/StressTest.h"
 
 int main()
 {
+    try {
+        StressTest test("127.0.0.1", 4242, 1000); // 1000명의 동시 접속 테스트
+        test.Run();
+    }
+    catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
     return 0;
 }
