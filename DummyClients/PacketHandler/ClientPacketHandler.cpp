5b38758a8c8ab123d9028f4f3e6553da151144c7
peace0096 <peace0096@naver.com>
2024-12-29 00:56:01 +0900

    [추가] 스트레스 테스트를 위한 뼈대 설계

---
+++ DummyClients/PacketHandler/ClientPacketHandler.cpp
@@ -0,0 +1,20 @@
+﻿#include "ClientPacketHandler.h"
+#include "LobbyPacketHandler.h"
+
+PacketHandlerFunc GPacketHandler[UINT16_MAX];
+
+bool Handle_INVALID(SessionPtr& session, asio::mutable_buffer& buffer, PacketHeader& header, int& offset)
+{
+    return false;
+}
+
+void ClientPacketHandler::Init()
+{
+    for (int i = 0; i < UINT16_MAX; i++)
+        GPacketHandler[i] = Handle_INVALID;
+
+    GPacketHandler[message::HEADER::LOGIN_RES] = [](SessionPtr& session, asio::mutable_buffer& buffer, PacketHeader& header, int& offset)
+    {
+        return HandlePacket<account::login::S_Login>(LobbyPacketHandler::Handle_S_Login, session, buffer, header, offset);
+    };
+}
