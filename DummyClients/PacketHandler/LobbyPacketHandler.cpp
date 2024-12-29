7adebb42051a239af6a4ee27a799fb4d818509d4
peace0096 <peace0096@naver.com>
2024-12-30 01:06:36 +0900

    [수정] 초기화 안해서 패킷 안 올라가던 점 수정..

--- DummyClients/PacketHandler/LobbyPacketHandler.cpp
+++ DummyClients/PacketHandler/LobbyPacketHandler.cpp
@@ -1,6 +1,21 @@
 ﻿#include "LobbyPacketHandler.h"
 
+#include "Session/Session.h"
+
 bool LobbyPacketHandler::Handle_S_Login(SessionPtr& session, account::login::S_Login& pkt)
 {
+    if (pkt.is_success())
+    {
+        account::character::select::C_CharacterList characterListPkt;
+
+        const size_t requiredSize = PacketUtil::RequiredSize(characterListPkt);
+
+        char* rawBuffer = new char[requiredSize];
+        auto sendBuffer = asio::buffer(rawBuffer, requiredSize);
+        PacketUtil::Serialize(sendBuffer, message::HEADER::CHARACTER_LIST_REQ, characterListPkt);
+
+        session->Send(sendBuffer);
+    }
+    
     return pkt.is_success();
 }
--- DummyClients/main.cpp
+++ DummyClients/main.cpp
@@ -1,10 +1,11 @@
-﻿#include "pch.h"
+#include "pch.h"
 #include "Test/StressTest.h"
 
 int main()
 {
+    ClientPacketHandler::Init();
     try {
-        StressTest test("127.0.0.1", 4242, 1000); // 1000명의 동시 접속 테스트
+        StressTest test("127.0.0.1", 4242, 100); // 1000명의 동시 접속 테스트
         test.Run();
     }
     catch (std::exception& e) {
