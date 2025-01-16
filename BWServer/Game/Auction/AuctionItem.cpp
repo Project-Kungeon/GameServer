cec47d910d7ad31403b80cb01873920e111bdab6
peace0096 <peace0096@naver.com>
2025-01-17 00:20:31 +0900

    [수정] (테스트 미완) 경매장 물품 구매 로직 작성 완료

--- BWServer/Game/Auction/AuctionItem.cpp
+++ BWServer/Game/Auction/AuctionItem.cpp
@@ -86,7 +86,7 @@ void AuctionItem::AsyncPurchased(PlayerPtr player, uint64 price, uint64 quantity
                 );
 
             // 사용자 테라 감소
-            std::string user_tera_update_sql = "UPDATE user_tera SET tera - ? "
+            std::string user_tera_update_sql = "UPDATE user_tera SET tera = tera - ? "
                                                 "WHERE user_pk = ?";
             SqlUtils::executeUpdate(conn->sql_connection, "GameDB", user_tera_update_sql,
                 purchased_item_quantity * price,
@@ -94,43 +94,82 @@ void AuctionItem::AsyncPurchased(PlayerPtr player, uint64 price, uint64 quantity
                 );
 
             // 경매장 물품 업데이트 및 삭제
-            std::string auction_item_update_sql = "UPDATE auction_items SET quantity - ? "
+            
+            std::string auction_item_update_sql = "UPDATE auction_items SET quantity = quantity - ? "
                                                 "WHERE pk = ?";
             std::string auction_item_delete_sql = "DELETE FROM auction_items "
                                                 "WHERE pk = ?";
+
+            // 구매자 혹은 판매자의 우편함에 물품 혹은 테라 발송
+            std::string mailbox_insert_sql = "INSERT INTO user_mailbox (user_pk, sender_name, title, content) VALUES (?, ?, ?, ?)";
+            std::string mailbox_item_insert_sql = "INSERT INTO mailbox_item (mail_pk, item_id, item_quantity) VALUES (? , ?, ?)";
+
+            // 구매자에게 우편 발송
+            SqlUtils::executeUpdate(conn->sql_connection, "GameDB", mailbox_insert_sql,
+                session->user_pk(),
+                "경매장",
+                "경매장 물품 구입",
+                "입찰 물품입니다.");
+            auto buyer_mail_rs = SqlUtils::executeQuery(conn->sql_connection, "GameDB", "SELECT LAST_INSERT_ID()");
+            
+            auto buyer_mail_id = 0;
+            if (buyer_mail_rs->next())
+                buyer_mail_id = buyer_mail_rs->getUInt64(1);
+            else
+                throw sql::SQLException("Mailbox does not exist.");
             
             for (auto& purchased_item : purchased_items)
             {
                 // 다 팔린 물품이라면, DB에서 제거
                 if (purchased_item.sold_out)
                 {
-                    
+                    SqlUtils::executeQuery(conn->sql_connection, "GameDB", auction_item_delete_sql,
+                        purchased_item.auction_id);
                 }
                 // 일부만 팔린 물품이라면, 업데이트
                 else
                 {
-                    
+                    SqlUtils::executeUpdate(conn->sql_connection, "GameDB", auction_item_update_sql,
+                        purchased_item.auction_id,
+                        session->user_pk());
                 }
+                SqlUtils::executeUpdate(conn->sql_connection, "GameDB", mailbox_insert_sql,
+                            purchased_item.user_id,
+                            "경매장",
+                            "경매장 물품 판매",
+                            "판매 결과입니다.");
+                auto seller_mail_rs = SqlUtils::executeQuery(conn->sql_connection, "GameDB", "SELECT LAST_INSERT_ID()");
+                auto seller_mail_id = 0;
+
+                if (seller_mail_rs->next())
+                    seller_mail_id = seller_mail_rs->getUInt64(1);
+                else
+                    throw sql::SQLException("Mailbox does not exist.");
+
+                
+                // 발송된 우편과 물품 연관관계
+                SqlUtils::executeUpdate(conn->sql_connection, "GameDB", mailbox_item_insert_sql,
+                    buyer_mail_id,
+                    item_table,
+                    purchased_item.quantity
+                    );
+
+                SqlUtils::executeUpdate(conn->sql_connection, "GameDB", mailbox_item_insert_sql,
+                    seller_mail_id,
+                    99, // 테라
+                    purchased_item.quantity * purchased_item.price
+                    );
             }
-
-            // 구매자 우편함에 물품 전송
-
-            // 판매자 우편함에 테라 전송
+            conn->sql_connection->commit();
+            
         }
         catch (sql::SQLException exception)
         {
             // TODO : DB 갱신 실패 시, 로그 남기건 다시 재시도 하던 해야함
             conn->sql_connection->rollback();
         }
-    
-        // 2.1 사용자 테라 정보 갱신
-    
-    
-
-        // 2.2 경매장 물품 정보 갱신
-    
-
-        // 4. 구매 기록 로그 저장
+        conn->sql_connection.reset();
+        GConnectionPool->unborrow(conn );
     }
     
 
