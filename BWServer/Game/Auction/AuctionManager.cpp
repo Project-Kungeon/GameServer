e1475e2734018910a2991899cbbf79c5a5ef10d0
peace0096 <peace0096@naver.com>
2025-01-17 13:27:27 +0900

    [추가] 경매장 유저 인터페이스 및 주기적 갱신 틀 추가

--- BWServer/Game/Auction/AuctionManager.cpp
+++ BWServer/Game/Auction/AuctionManager.cpp
@@ -4,28 +4,24 @@
 // std::shared_ptr<AuctionManager> GAuctionManager = std::make_shared<AuctionManager>();
 
 AuctionManager::AuctionManager(asio::io_context& io_context)
-    : _io_context(io_context)
+    : _io_context(io_context), JobQueue(io_context)
 {
+    Init();
 }
 
 AuctionManager::~AuctionManager()
 {
 }
 
-bool AuctionManager::registerItem(auction::S_PutItem& pkt)
+bool AuctionManager::applyItem(PlayerPtr player, auction::S_PutItem& pkt)
 {
-    // TODO : 캐릭터가 아이템 소유 중인지 체크
-    
-    return true;
 }
 
-bool AuctionManager::purchaseItem(auction::S_PurchaseItem& pkt)
+bool AuctionManager::purchaseItem(PlayerPtr player, auction::S_PurchaseItem& pkt)
 {
-    // 1. 사용자가 테라 소유 중인지 체크
+}
 
-    // 2. Auction Item 처리
-
-    
-    
-    return true;
+void AuctionManager::Init()
+{
+    // 경매장 물품 초기화
 }
--- BWServer/Game/Auction/AuctionManager.h
+++ BWServer/Game/Auction/AuctionManager.h
@@ -1,15 +1,18 @@
 ﻿#pragma once
 
-class AuctionManager
+class AuctionManager : public JobQueue
 {
 public:
     AuctionManager(asio::io_context& io_context);
     ~AuctionManager();
     
-    bool registerItem(auction::S_PutItem& pkt);
-    bool purchaseItem(auction::S_PurchaseItem& pkt);
+    bool applyItem(PlayerPtr player, auction::S_PutItem& pkt);
+    bool purchaseItem(PlayerPtr player, auction::S_PurchaseItem& pkt);
+
 private:
-    USE_LOCK;
+    void Init();
+    
+private:
     // 옥션 아이템마다 strand 지정을 위한 io context 참조
     asio::io_context& _io_context;
     map<uint64, AuctionItemPtr> _AuctionItems;
